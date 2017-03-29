5363aeecc5afcd9c45640761ee5a0b47a4ba7c7c
Pathum Athukorala <pathumathukorala@gmail.com>
2017-03-29 16:02:25 +0200

    add the config and css related to alert notification

---
+++ config/lanmaster.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tenant: "lanmaster",
+  defaultLanguage : "sv",
+  alert: {
+    baseUrl: "https://apigw.ebuilder.io/webjourney-notify/",
+    version: "v1-0",
+    mapping: "/alert",
+    timeout: 3000
+  }
+};
